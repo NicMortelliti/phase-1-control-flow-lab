f703402d0d818bd8138849d9411150ff879b5b64
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-01-31 15:27:34 -0800

    commit tree a35f5df4553a901acd2bc34f1e0b5e2c9cd232cf

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const cityNyc = "NYC";
+
+let ride;
+
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return "This one is on me!";
+  }
+  else if (2000 < ride && ride <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === cityNyc ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (true){
+    case tip === "generous":
+      return "Thank you so much.";
+      break;
+    case tip === "not as generous":
+      return "Thank you.";
+      break;
+    case tip === "thanks for everything":
+      return "Bye.";
+      break;
+  }
 }
